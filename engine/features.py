a15bb4bab80df7ca703170da673b491d399bac7e
Steven Gabriel Madali <88831228+Valiantic@users.noreply.github.com>
2024-02-13 22:17:40 +0800

    Update features.py

--- engine/features.py
+++ engine/features.py
@@ -1,5 +1,8 @@
 import os
 import re
+import sqlite3
+import webbrowser
+import pyjokes
 from playsound import playsound
 import eel
 from engine.command import speak 
@@ -12,22 +15,50 @@ import pywhatkit as kit
 
 from engine.helper import extract_yt_term, remove_words
 
+conn = sqlite3.connect("clara.db")
+cursor = conn.cursor()
 
 @eel.expose #somehow not working with live server
 def playAssistantSound():
     music_dir = "www\\assets\\audio\\start_sound.mp3"
     playsound(music_dir)
     
-def openCommand(query):
+def openCommand(query): # opening apps and website 
     query = query.replace(ASSISTANT_NAME, "")
     query = query.replace("open","")
     query.lower()
     
-    if query!="":
-        speak("Got it, Opening "+query)
-        os.system('start '+query)
-    else:
-        speak("not found")
+    app_name = query.strip()
+    
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            results = cursor.fetchall()
+
+            if len(results) != 0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+
+            elif len(results) == 0: 
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                results = cursor.fetchall()
+                
+                if len(results) != 0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("not found")
+        except:
+            speak("some thing went wrong")
+
         
 # def PlayYoutube(query): # function to play youtube on chrome
 #     search_term = extract_yt_term(query)
@@ -39,8 +70,11 @@ def openCommand(query):
 #     match = re.search(pattern, command, re.IGNORECASE)
 #     return match.group(1) if match else None
 
-def PlayYoutube(query):
+def PlayYoutube(query): # to play videos on youtube
     search_term = extract_yt_term(query)
     speak("Playing "+search_term+" on YouTube")
     kit.playonyt(search_term)
+    
+def joke(query = ''): # humor of clara
+    pyjokes.get_joke('en','neutral', max_tokens= 50) 
 
