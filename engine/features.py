98190d82e634a58e534bf757eddf610a0095a480
Steven Gabriel Madali <88831228+Valiantic@users.noreply.github.com>
2024-02-17 23:43:26 +0800

    Update features.py

--- engine/features.py
+++ engine/features.py
@@ -1,7 +1,10 @@
 import os
 import re
 import sqlite3
+import struct
+import time
 import webbrowser
+import pyaudio
 import pyjokes # joke function
 from playsound import playsound
 import eel
@@ -12,6 +15,7 @@ from engine.config import ASSISTANT_NAME
 
 # intro play assistant function
 import pywhatkit as kit
+import pvporcupine
 
 from engine.helper import extract_yt_term, remove_words
 
@@ -78,6 +82,48 @@ def PlayYoutube(query): # to play videos on youtube
     
 def joke(query = ''): # humor of clara
     pyjokes.get_joke('en','neutral', max_tokens= 50) 
+    
+
+# def hotword():  # NOT APPLICABLE DUE TO PRE TRAINED WORDS
+#     porcupine=None
+#     paud=None
+#     audio_stream=None
+#     try:
+       
+#         # pre trained keywords    
+#         porcupine=pvporcupine.create(keywords=["clara","alexa"]) 
+#         paud=pyaudio.PyAudio()
+#         audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
+        
+#         # loop for streaming
+#         while True:
+#             keyword=audio_stream.read(porcupine.frame_length)
+#             keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
+
+#             # processing keyword comes from mic 
+#             keyword_index=porcupine.process(keyword)
+
+#             # checking first keyword detetcted for not
+#             if keyword_index>=0:
+#                 print("hotword detected")
+
+#                 # pressing shorcut key win+j
+#                 import pyautogui as autogui
+#                 autogui.keyDown("win")
+#                 autogui.press("j")
+#                 time.sleep(2)
+#                 autogui.keyUp("win")
+                
+#     except:
+#         if porcupine is not None:
+#             porcupine.delete()
+#         if audio_stream is not None:
+#             audio_stream.close()
+#         if paud is not None:
+#             paud.terminate()
+
+
+
     
 
 
