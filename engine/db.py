d38a530b85325e207cd6b11b2f1994ccd81f8c6e
Steven Gabriel Madali <88831228+Valiantic@users.noreply.github.com>
2024-02-19 00:43:11 +0800

    Update db.py

--- engine/db.py
+++ engine/db.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 conn = sqlite3.connect("clara.db")
@@ -23,4 +24,31 @@ cursor.execute(query)
 # cursor.execute("DELETE FROM web_command WHERE id = ")
 # conn.commit()
 
+# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
 
+# Specify the column indices you want to import (0-based index)
+# Example: Importing the 1st and 3rd columns
+# desired_columns_indices = [0, 32] # WHATEVER YOU GOT FROM THE EXCEL COMMAND =COLUMN(LASTCOLUMN+1) = MINUS THE RESULT TO 1
+
+# # Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+
+# # Commit changes and close connection
+# conn.commit()
+# conn.close()
+
+# INSERTING A SINGLE ROW FOR A NEW CONTACT
+# query = "INSERT INTO contacts VALUES (null,'pawan', '1234567890', 'null')"
+# cursor.execute(query)
+# conn.commit()
+
+# query = 'kunal'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(results[0][0]) # INDEX OUT OF RANGE
