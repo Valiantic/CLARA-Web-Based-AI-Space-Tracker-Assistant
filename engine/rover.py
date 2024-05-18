c246902f3d3c3231634d512449d29c38292d74af
Steven Gabriel Madali <88831228+Valiantic@users.noreply.github.com>
2024-05-18 23:42:08 +0800

    Add files via upload

---
+++ engine/rover.py
@@ -0,0 +1,34 @@
+import requests
+import random
+import webbrowser
+from engine.command import speak 
+
+api_key = 'Pa41EKygcE2L3n3DImI43fXEc445gGyKqwYQO2k4'
+api_url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
+
+def get_random_photo():
+            params = {
+                'api_key': api_key,
+                'sol': random.randint(0, 1000),  # choose a random Martian sol between 0 and 1000
+            }
+            response = requests.get(api_url, params=params)
+            response.raise_for_status()  # ensure we got a successful response
+            photos = response.json()['photos']
+
+            if not photos:
+                print("No photos found for the chosen Martian sol. Try again.")
+                return None
+
+            photo = random.choice(photos)  # choose a random photo
+            return photo
+
+def mainrover(query):
+            photo = get_random_photo()
+            if photo is not None:
+                webbrowser.open(photo['img_src'])
+                print(f"Displaying a random photo taken by the {photo['rover']['name']} rover on Martian sol {photo['sol']} using the {photo['camera']['full_name']} ({photo['camera']['name']}).")
+                speak(f"Here's a random photo taken by the {photo['rover']['name']} rover on Martian sol {photo['sol']} using the {photo['camera']['full_name']} ({photo['camera']['name']}).")
+        
+
+# if __name__ == '__main__':
+#             mainrover()
